fb4fa4de8f18577d9708d03a6201b89e826e1fdd
Master <baxarev.777@yandex.ru>
2021-09-02 19:37:08 +0300

    Выполнение домашней работы

--- src/js/GetRandomCell.js
+++ src/js/GetRandomCell.js
@@ -1,5 +1,8 @@
 export default function getRandomCell() {
   const gameCells = document.querySelectorAll('.gameboard__cell');
-  const randomCell = gameCells[Math.round(gameCells.length * Math.random())];
+  let randomCell = gameCells[Math.round(gameCells.length * Math.random())];
+  while (!randomCell) {
+    randomCell = gameCells[Math.round(gameCells.length * Math.random())];
+  }
   return randomCell;
 }
